d8cac17f79f91ec13112d537261f016efccb9ee0
nataleedesotell <nataleerobson@gmail.com>
2018-01-23 21:38:36 -0500

    Done with Q4, close to a solution on Q5

--- DESOTELL_HW1_P4.py
+++ DESOTELL_HW1_P4.py
@@ -2,62 +2,30 @@
 #NATALEE DESOTELL
 #HPM 573 HOMEWORK 1
 
-#the end of lecture 5 is helpful for this
+# 1. Describe how ours1 and ours2 differ from each other.
+
+#"Append" adds an object to the list, but "+" combines two lists
+# and sets a new list = to a variable (ours1). Ours2 will continue
+#to reference the old lists, while ours1 is an entirely new list.
+
+# 2. Explain why changing yours would changes ours2 but not ours1.
+
+#Using "+" created a new list that doesn't continue to reference the
+#old lists. However, .append continues to reference the original lists,
+#meaning it will be changed when the original lists are changed. Hence, the
+#change in ours2 while ours1 stayed the same.
 
 #PART 1
-#create a list named yours and store fave schools
 yours = ["Yale", "MIT", "Berkeley"]
-#each of these schools is an attribute and they are changeable
-
-#create a list named mine with my face schools
 mine = ["UW Madison", "UT Austin", "Madison College"]
-
-#use the + operator to create a new list, named ours1, to represent our fave schools
 ours1 = mine + yours
-
-#now, create another list ours2 to again represent our fave schools but use:
 ours2 = []
 ours2.append(mine)
-#add things to the end of list
 ours2.append(yours)
-
 print(ours1)
 print(ours2)
 
+yours[2] = "MSOE"
 
-#ours1 and ours2 differ how?
-print(type(ours1))
-print(type(ours2))
-
-#tuples are immutable
-#lists are muutable objects
-
-#can add elements to end of list with L.append(element)
-#mutates the list
-
-#yours[2] = "MSOE" #is this how I change element 2?
-#side effect of mutating a list
-
-
-
-#the plus operator gives a new list that's the sum of the 2 lists
-#does not mutate the list
-#gives a new list, doesn't change the original lists
-
-#we have to assign the result of the addition to the new list otherwise the result is lost
-#mutate a list directly: use extend
-
-#the list that gets mutated is the one that's passed into the (argument?)
-
-
-
-
-# #PART 2
-# yours = ["Yale", "MIT", "MSOE"]
-# mine = ["UW Madison", "UT Austin", "Madison College"]
-
-#second element is the third in the list
-# print(ours1)
-# print(ours2)
-
-#why does changing yours change ours2 but not ours1?
+print("After changing 'yours':", ours1)
+print("After changing 'yours':", ours2)
--- DESOTELL_HW1_P5.py
+++ DESOTELL_HW1_P5.py
@@ -2,10 +2,9 @@
 #NATALEE DESOTELL
 #HPM 573 HOMEWORK 1
 
-#Create a dictionary for the months of the year that can be 
-#called by the number of months (i.e. 1, 2, ..., 12) or the 
-#name of months (i.e. January, February, ..., December). 
-#Write a statement that prints The sixth month is June
-#and another statement that prints February is month 2.
+#create dictionary
+months = {'January': 1 ,'February': 2 ,'March': 3 ,'April': 4 ,'May':5,'June':6, 'July':7,'August':8,'September':9,'October':10,'November':11,'December':12}
 
-months = {'January':1,'February':2,'March':3,'April':4,'May':5,'June':6, 'July':7,'August':8,'September':9,'October:10','November':11,'December':12}
+print("The sixth month is", months[2]) #ERROR: I can't call the key from the value, but I'm not sure how else to do it.
+print("February is month", months['February']) #calling the value from the key
+
