7d1b564a75a0f4b5e89cfa5c0714a5644b4acf84
nataleedesotell <nataleerobson@gmail.com>
2018-01-23 23:15:34 -0500

    Q5 complete, final commit

--- DESOTELL_HW1_P5.py
+++ DESOTELL_HW1_P5.py
@@ -2,9 +2,7 @@
 #NATALEE DESOTELL
 #HPM 573 HOMEWORK 1
 
-#create dictionary
-months = {'January': 1 ,'February': 2 ,'March': 3 ,'April': 4 ,'May':5,'June':6, 'July':7,'August':8,'September':9,'October':10,'November':11,'December':12}
+months = {'January': 1 ,'February': 2 ,'March': 3 ,'April': 4 ,'May':5,'June':6, 'July':7,'August':8,'September':9,'October':10,'November':11,'December':12, 1:'January',2:'February',3:'March',4:'April',5:'May',6:'June',7:'July',8:'August',9:'September',10:'October',11:'November',12:'December'}
 
-print("The sixth month is", months[2]) #ERROR: I can't call the key from the value, but I'm not sure how else to do it.
-print("February is month", months['February']) #calling the value from the key
-
+print("The", months['June'],"th month is", months[2])
+print(months[2],"is month", months['February'])
